e130eb295a664952923a1832eee2a7131d89ce40
9co9co <niaa004@student.kristiania.no>
2025-02-26 16:21:17 +0100

    Project Folder
    Dette er hele Prosjekt-mappen. Åpne denne i Unity

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+   public Transform gunTip;
+   public GameObject bulletPrefab;
+   public float bulletSpeed = 20f;
+   public GameObject muzzleFlashPrefab;
+   
+}
